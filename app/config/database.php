2e335445c37416de7add0926f06f31a70b8d8bc3
Miko Joshua A. Dela Cruz <delacruz.mikoa@minsu.edu.ph>
2026-09-02 20:45:25 +0800

    update

--- app/config/database.php
+++ app/config/database.php
@@ -57,6 +57,19 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 |   Example: $database['another_example'] = array('key' => 'value')
 */
 
+// $database['main'] = array(
+//     'driver'	=> '',
+//     'hostname'	=> getenv('DB_HOST') ?: 'localhost',
+//     'port'		=> getenv('DB_PORT') ?: '3306',
+//     'username'	=> getenv('DB_USERNAME') ?: 'root',
+//     'password'	=> getenv('DB_PASSWORD') ?: '123',
+//     'database'	=> getenv('DB_NAME') ?: 'mydb',
+//     'charset'	=> '',
+//     'dbprefix'	=> '',
+//     // Optional for SQLite
+//     'path'      => ''
+// );
+
 $database['main'] = array(
     'driver'	=> '',
     'hostname'	=> getenv('DB_HOST') ?: '',
